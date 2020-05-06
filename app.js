f29e37e267ac0e570373c641b529732b03cb7723
John Brian <johnbryanjb011@gmail.com>
2020-05-06 06:28:05 +0300

    add user model

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ var express = require("express");
 var app = express();
 var ejs = require ("ejs");
 var navRoutes = require("./routes/nav");
-var port =8080
+var port =5050
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/kitchen",(err,db)=>{
+    if(err){
+        console.log(`error connecting to db ${err}`)
+    }
+    else{
+        console.log("database succesfully connected")
+    }
+})
 
 
 //set 
