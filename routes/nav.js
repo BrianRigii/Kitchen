286a512b093d75b7a76b3a68d4eac89c697363c0
John Brian <johnbryanjb011@gmail.com>
2020-05-17 11:44:07 +0300

    implemented node-fetch

--- routes/nav.js
+++ routes/nav.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/user")
+var getREC= require("../js/api")
 
 router.get("/",(req,res)=>{
     res.render("login")
 })
 
 router.get("/index",(req,res)=>{
+    getREC()
     res.render("index",{user :User})
 })
 
