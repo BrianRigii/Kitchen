7ec2a415c7bcb749fdd20f841c297c908d6c8736
John Brian <johnbryanjb011@gmail.com>
2020-05-18 05:07:58 +0300

    added login form

---
+++ public/jquery/registration.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+ $("#login").click(function(){
+     $(".form-log").removeClass("hide")
+     $(".form-sign").addClass("hide")
+ })
+
+ $("#Sign").click(function(){
+    $(".form-log").addClass("hide")
+    $(".form-sign").removeClass("hide")
+})
+
+
+})
+    
